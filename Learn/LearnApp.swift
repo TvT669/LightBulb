0fcbadd67183f902dc247c09eb4653db3c42e3de
TvT669 <3172736837@qq.com>
2025-02-25 14:13:26 +0800

    Initial Commit

---
+++ Learn/LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnApp.swift
+//  Learn
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
